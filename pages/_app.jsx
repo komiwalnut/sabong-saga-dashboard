6234984e2392c784761ca3bd103df73e91667a10
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-01-27 12:32:42 +0800

    Added Vercel analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,14 @@
 import '../index.css';
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
