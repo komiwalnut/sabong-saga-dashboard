04b71502816b01a527f8b9cf08b2e6547faf4f50
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-01-30 10:21:13 +0800

    analytics stuff for vercel

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,13 @@
 import '../index.css';
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <Component {...pageProps} />
     </>
   );
