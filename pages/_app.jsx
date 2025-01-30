6e8084bcf4449bf1abd82832c92d0611b1199421
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-01-30 18:58:31 +0800

    Fixed Vercel Speed Analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import '../index.css';
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }) {
   return (
